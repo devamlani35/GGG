bde642d49801e12695778851586a4a04e1629db3
Dev Amlani <amlanid35@gmail.com>
2023-02-01 06:50:11 -0500

    Adds new dataset class

--- work/ImDataset.py
+++ work/ImDataset.py
@@ -1,17 +1,18 @@
 import numpy as np
+import torch
 from sklearn.model_selection import train_test_split
 import os
 import mysql.connector
 import boto3
 
+client = boto3.client('s3')
+
 class ImDataset():
-    ENDPOINT = "gnome-1.cwqqmyn32oy5.us-east-1.rds.amazonaws.com"
-    PORT = "3306"
-    USER = "dev"
+
     REGION = "us-east-1a"
-    DBNAME = "train"
+
     os.environ['LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN'] = '1'
-    def __init__(self):
+    def __init__(self,mode, db_table, db_host, bucket_arn):
         num_files = 167585
         self.batch_size = 10
         self.file_indices = np.arange(0,num_files,1)
@@ -26,5 +27,18 @@ class ImDataset():
         return self.query_db(self.train_indices[start:start+10])
 
     def query_db(self,idxs):
-        
+        conn = mysql.connector.connect(host="gnome-1.cwqqmyn32oy5.us-east-1.rds.amazonaws.com", user="dev", password="gnome123", port="3306", database="train")
+        cur = conn.cursor()
+        for idx in range(idxs):
+            cur.execute("SELECT * FROM data_paths WHERE id={};".format(idx))
+        query_results = cur.fetchall()
+        X_paths = list(map(lambda x: x[1], query_results))
+        y_labels = torch.Tensor(list(map(lambda x: x[-1], query_results)))
+        X = torch.Tensor()
+        for val in X_paths:
+            res =
+            temp_x = torch.Tensor(res["data"])
+            X.add(temp_x)
+        return X, y_labels
+
 
