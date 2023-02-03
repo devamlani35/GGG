c565e7a754a91ee766a42d4f3f7ea5863d56b3c6
Dev Amlani <amlanid35@gmail.com>
2023-02-03 08:53:44 -0500

    Transfer finished model to ec2 for testing/training

--- work/connect_s3.py
+++
@@ -1,5 +0,0 @@
-import boto3
-
-client = boto3.client('s3')
-res = client.get_object(Bucket="arn:aws:s3-object-lambda:us-east-1:068469771374:accesspoint/s3-lambda-3c2", Key = "n01514668/n01514668_10004.JPEG")
-print(res)
