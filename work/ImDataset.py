10785dbef552e724ec31fa59aa513c8649cd1bd3
Ubuntu <ubuntu@ip-172-31-36-49.ec2.internal>
2023-02-06 15:46:10 +0000

    Fix batch shape

--- work/ImDataset.py
+++ work/ImDataset.py
@@ -15,7 +15,7 @@ class ImDataset():
 
     def __init__(self,mode="part1", db_table="data_paths", db_host="gnome-1.cwqqmyn32oy5.us-east-1.rds.amazonaws.com", bucket_arn="arn:aws:s3-object-lambda:us-east-1:068469771374:accesspoint/s3-lambda-3c2"):
         num_files = 167585
-        self.batch_size = 10
+        self.batch_size = 3
         self.file_indices = np.arange(0,num_files,1)
         self.train_indices, self.val_indices = train_test_split(self.file_indices, train_size=0.9, test_size=0.1)
         self.table = db_table
@@ -48,15 +48,17 @@ class ImDataset():
             query_results.append(cur.fetchall()[0])
         X_paths = list(map(lambda x: x[1], query_results))
         y_labels = torch.Tensor(list(map(lambda x: x[-1], query_results)))
-        X = list()
+        X = []
         for val in X_paths:
-            print(val)
             res = client.get_object(Bucket=self.arn, Key = val)
             img_arr = np.frombuffer(res["Body"].read(), dtype=np.float32)
             img_arr = img_arr.reshape((720,1280, 3))
             t_img = img_arr.copy()
             X.append(t_img)
+            print(val)
+        X = np.array(X)
         X = torch.Tensor(X)
+        X = torch.permute(X, (0,3,1,2))
         return X, y_labels
 
 
